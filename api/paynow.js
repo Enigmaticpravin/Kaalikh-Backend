0699440202751de8f02fcadd4528c38c017a52b1
Pravin <pravingupta2020@gmail.com>
2024-10-23 17:27:56 +0530

    "Refactor PayU payment API to use crypto module instead of CryptoJS, simplify configuration, and update payment data handling"

--- api/paynow.js
+++ api/paynow.js
@@ -1,60 +1,45 @@
-require('dotenv').config();
-const CryptoJS = require("crypto-js");
+const crypto = require('crypto');
 
-// PayU Configuration
-const payuConfig = {
-  merchantKey: process.env.PAYU_MERCHANT_KEY,
-  merchantSalt: process.env.PAYU_MERCHANT_SALT,
-  payuBaseUrl: process.env.PAYU_BASE_URL,
-  successUrl: process.env.SUCCESS_URL,
-  failureUrl: process.env.FAILURE_URL,
-};
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    // Extract form data from request body
+    const { email, phone } = req.body;
+    const txnId = crypto.randomBytes(8).toString('hex');
+    
+    const paymentData = {
+      key: process.env.PAYU_MERCHANT_KEY,
+      txnid: txnId,
+      amount: 160,
+      productinfo: 'Kaalikh Book',
+      firstname: "Nymoria Press",
+      email: email,
+      phone: phone,
+      surl: process.env.SUCCESS_URL,
+      furl: process.env.FAILURE_URL
+    };
 
-// Generate hash using SHA512
-function generateHash(data) {
-    const hashString = `${payuConfig.merchantKey}|${data.txnid}|${data.amount}|${data.productinfo}|${data.firstname}|${data.email}|||||||||||${payuConfig.merchantSalt}`;
-    return CryptoJS.SHA512(hashString).toString();
+    // Generate hash
+    const hashString = `${paymentData.key}|${paymentData.txnid}|${paymentData.amount}|${paymentData.productinfo}|${paymentData.firstname}|${paymentData.email}|||||||||||${process.env.PAYU_MERCHANT_SALT}`;
+    const hash = crypto.createHash('sha512').update(hashString).digest('hex');
+    paymentData.hash = hash;
+
+    // Send HTML form response for PayU
+    res.status(200).send(`
+      <form id="payuForm" method="post" action="${process.env.PAYU_BASE_URL}">
+        <input type="hidden" name="key" value="${paymentData.key}">
+        <input type="hidden" name="txnid" value="${paymentData.txnid}">
+        <input type="hidden" name="amount" value="${paymentData.amount}">
+        <input type="hidden" name="productinfo" value="${paymentData.productinfo}">
+        <input type="hidden" name="firstname" value="${paymentData.firstname}">
+        <input type="hidden" name="email" value="${paymentData.email}">
+        <input type="hidden" name="phone" value="${paymentData.phone}">
+        <input type="hidden" name="surl" value="${paymentData.surl}">
+        <input type="hidden" name="furl" value="${paymentData.furl}">
+        <input type="hidden" name="hash" value="${paymentData.hash}">
+      </form>
+      <script type="text/javascript">document.getElementById('payuForm').submit();</script>
+    `);
+  } else {
+    res.status(405).send({ message: 'Only POST requests allowed' });
   }
-
-// Generate a unique transaction ID
-function generateTxnId() {
-  return crypto.randomBytes(8).toString('hex');
 }
-
-// Handle the PayU payment request
-module.exports = (req, res) => {
-  const txnId = generateTxnId();
-  const paymentData = {
-    key: payuConfig.merchantKey,
-    txnid: txnId,
-    amount: 160,
-    productinfo: 'Kaalikh Book',
-    firstname: 'Nymoria Press',
-    email: req.body.email,
-    phone: req.body.phone,
-    surl: payuConfig.successUrl,
-    furl: payuConfig.failureUrl,
-  };
-
-  paymentData.hash = generateHash(paymentData);
-
-  // Redirect the user to the PayU payment page
-  const form = `
-    <form id="payuForm" method="post" action="${payuConfig.payuBaseUrl}">
-      <input type="hidden" name="key" value="${paymentData.key}">
-      <input type="hidden" name="txnid" value="${paymentData.txnid}">
-      <input type="hidden" name="amount" value="${paymentData.amount}">
-      <input type="hidden" name="productinfo" value="${paymentData.productinfo}">
-      <input type="hidden" name="firstname" value="${paymentData.firstname}">
-      <input type="hidden" name="email" value="${paymentData.email}">
-      <input type="hidden" name="phone" value="${paymentData.phone}">
-      <input type="hidden" name="surl" value="${paymentData.surl}">
-      <input type="hidden" name="furl" value="${paymentData.furl}">
-      <input type="hidden" name="hash" value="${paymentData.hash}">
-    </form>
-    <script type="text/javascript">
-      document.getElementById('payuForm').submit();
-    </script>
-  `;
-  res.send(form);
-};
